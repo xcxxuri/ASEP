700b186fc168a9a29beb4af15f35c6f0e51e09f4
xcxxuri <1157850364@qq.com>
2023-03-27 20:10:25 -0700

    Update Assignment1.cs
    add comments

--- week2/Mar27/Assignment1.cs
+++ week2/Mar27/Assignment1.cs
@@ -3,18 +3,23 @@ public class Solution
     // leetcode 1
     public int[] TwoSum(int[] nums, int target)
     {
-        var dict = new Dictionary<int, int>();
+        // create a dictionary to store the number and its index
+        dict = new Dictionary<int, int>();
         for (int i = 0; i < nums.Length; i++)
         {
+            // if the dictionary contains the key of target - nums[i],
             if (dict.ContainsKey(target - nums[i]))
             {
+                // return the index of the key and the current index
                 return new int[] { dict[target - nums[i]], i };
             }
             else
             {
+                // otherwise, add the number and its index to the dictionary
                 dict[nums[i]] = i;
             }
         }
+        // if no solution found, return empty array
         return new int[] { };
     }
 
@@ -22,13 +27,17 @@ public class Solution
     // leetcode 9 
     public bool IsPalindrome(int x)
     {
+        // if the number is negative, return false
         if (x < 0)
         {
             return false;
         }
-        var str = x.ToString();
+        // convert the number to string
+        string str = x.ToString();
+        // compare the first and last character, second and second last character, etc.
         for (int i = 0; i < str.Length / 2; i++)
         {
+            // if the characters are not the same, return false
             if (str[i] != str[str.Length - 1 - i])
             {
                 return false;
@@ -40,15 +49,19 @@ public class Solution
     // leetcode 217
     public bool ContainsDuplicate(int[] nums)
     {
+        // create a dictionary to store the number and its index
         var dict = new Dictionary<int, int>();
+        // iterate through the array
         for (int i = 0; i < nums.Length; i++)
         {
+            // if the dictionary contains the number, return true
             if (dict.ContainsKey(nums[i]))
             {
                 return true;
             }
             else
             {
+                // otherwise, add the number and its index to the dictionary
                 dict[nums[i]] = i;
             }
         }
@@ -58,21 +71,26 @@ public class Solution
     // leetcode 412
     public IList<string> FizzBuzz(int n)
     {
+        // create a list to store the result
         var list = new List<string>();
         for (int i = 1; i <= n; i++)
         {
+            // if the number is divisible by 3 and 5, add "FizzBuzz" to the list
             if (i % 3 == 0 && i % 5 == 0)
             {
                 list.Add("FizzBuzz");
             }
+            // if the number is divisible by 3, add "Fizz" to the list
             else if (i % 3 == 0)
             {
                 list.Add("Fizz");
             }
+            //  if the number is divisible by 5, add "Buzz" to the list
             else if (i % 5 == 0)
             {
                 list.Add("Buzz");
             }
+            // otherwise, add the number to the list
             else
             {
                 list.Add(i.ToString());
@@ -80,5 +98,5 @@ public class Solution
         }
         return list;
     }
-    
+
 }

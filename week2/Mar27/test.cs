09f0ad6bd3ba16aaae042cb345f004bdfc5f1aa3
xcxxuri <1157850364@qq.com>
2023-03-27 18:58:19 -0700

    test upload
    test upload

---
+++ week2/Mar27/test.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");    
+
+      var[] arr2 = new int[] { 1, 2, 3 };
+    }
+  }
+}
